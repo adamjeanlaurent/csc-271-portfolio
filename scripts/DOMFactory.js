6a7b72fe1304b14ef57301e84382c131fc875f23
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2021-02-18 11:59:56 -0500

    testing url params

--- scripts/DOMFactory.js
+++ scripts/DOMFactory.js
@@ -3,6 +3,7 @@ import getSocks from './getSocks';
 export const renderAllSocks = () => {
   const socks = getSocks();
       for(let sock of socks) {
+          const containerDiv = document.createElement('div');
           // create elements
           // append elements to page
       }
