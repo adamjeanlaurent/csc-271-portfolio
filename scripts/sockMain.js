34c9a33838cf8144ea1e5d86c44f7f385fead1f7
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2021-03-16 11:59:21 -0400

    stuff

--- scripts/sockMain.js
+++ scripts/sockMain.js
@@ -3,7 +3,7 @@ import { addToCart, showAddedToCartAlert } from './addToCart.js';
 $(document).ready(() => {
     const addToCardButton = document.getElementById('addToCart');
     addToCardButton.addEventListener('click', (e) => {
-    addToCart(e);
-    showAddedToCartAlert(true);
+        addToCart(e);
+        showAddedToCartAlert(true);
     });
 });
