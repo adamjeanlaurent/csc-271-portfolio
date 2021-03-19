4bab72b04572d8a4f398c308caea4a1096605546
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2021-03-19 11:08:29 -0400

    refactor done, buy page still weird but it is what it is

--- server.js
+++ server.js
@@ -9,21 +9,59 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    return res.render('index');
+    return res.redirect('/index');
+});
+
+app.get('/index', (req, res) => {
+    return res.render('index', {
+        styles: [
+            '../styles/index.css',
+            '../styles/footer.css',
+            '../styles/homepage.css'
+        ]
+    });
 });
 
 app.get('/sock/:sockId', (req, res) => {
     const { sockId } = req.params;
     const sock = getSocks(sockId);
-    return res.render('sock', { sock: sock });
+    return res.render('sock', { 
+        sock: sock,
+        styles: [
+            '../styles/footer.css',
+            '../styles/index.css',
+            '../styles/sock.css'
+        ]
+     });
 });
 
 app.get('/buy', (req, res) => {
-    return res.render('buy');
+    return res.render('buy', {
+        styles: [
+            '../styles/index.css',
+            '../styles/footer.css'
+        ] 
+    });
 });
 
 app.get('/cart', (req, res) => {
-    return res.render('cart');
+    return res.render('cart', {
+        styles: [
+            '../styles/index.css',
+            '../styles/footer.css',
+            '../styles/cart.css'
+        ]
+    });
+});
+
+app.get('/*', (req, res) => {
+    res.status(404);
+    return res.render('404', {
+        styles: [
+            '../styles/index.css',
+            '../styles/footer.css',
+        ]
+    });
 });
 
 app.listen(3000);
